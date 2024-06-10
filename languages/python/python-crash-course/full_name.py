28b65d8b67773c8625b7c8cb201a8c876e6229a1
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2024-06-10 23:19:09 +0800

    Starting learning python

---
+++ languages/python/python-crash-course/full_name.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+first_name = "quazi"
+last_name = 'ashfaq'
+full_name = f'{first_name} {last_name}'
+print(full_name.title())
---
+++ languages/python/python-crash-course/simple_message.py
@@ -0,0 +1,7 @@
+message = 'welcome to world!'
+print(message)
+message = 'Testing the communication channel'
+print(message)
+
+name = 'quazi ashfaq'
+print(name.title())
