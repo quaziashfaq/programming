5cc545fc56a88c2734f754ca0aa7b883cb16c7ec
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2024-06-29 08:54:00 +0800

    refactored AlienInvasion.run_game function

--- languages/python/python-crash-course/general/alien_invasion/alien_invasion.py
+++ languages/python/python-crash-course/general/alien_invasion/alien_invasion.py
@@ -22,16 +22,25 @@ class AlienInvasion:
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                # Redraw the screen druing each pass through the loop.
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
-                # Make the most recently drawn screen visible.
-                pygame.display.flip()
-                self.clock.tick(60)
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        '''Watch for keyboard and mouse events.'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        '''Update images on the screen, and flip to the new screen.'''
+        # Redraw the screen druing each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
