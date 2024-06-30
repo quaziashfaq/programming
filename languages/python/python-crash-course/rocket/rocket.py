ad926fae35f66f44b51c42affcbacedb30fc3537
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2024-06-30 11:27:40 +0800

    Adding Q to quit to Rocket game.

--- languages/python/python-crash-course/rocket/rocket.py
+++ languages/python/python-crash-course/rocket/rocket.py
@@ -8,19 +8,23 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
+        self.bg_color = (128, 128, 128)
 
 class RocketGame:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        self.bg_color = (128, 128, 128)
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        sys.exit()
                 
             self.screen.fill(self.bg_color)
             pygame.display.flip()
