979563c09f17c6a11643cd14f1e2e4c39cc27669
Quazi Ashfaq Ur Rahman <quazi.ashfaq@gmail.com>
2024-09-11 00:28:33 +0800

    Earthquake data python file

--- languages/python/python-crash-course/earthquake_data/eq_explore_data.py
+++ languages/python/python-crash-course/earthquake_data/eq_explore_data.py
@@ -2,8 +2,11 @@
 
 from pathlib import Path
 import json
+import os
 
-path = Path('./eq_data_1_day_m1.geojson')
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path = Path(dir_path + '/eq_data_1_day_m1.geojson', encoding='utf8')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
